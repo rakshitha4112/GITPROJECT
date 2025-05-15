921d27643a3c0a31b13b157e33c32c9783170f87
Alankrutha-V <alankruthav02@gmail.com>
2025-05-15 14:54:07 +0530

    20

--- Demo.java
+++ Demo.java
@@ -1,7 +1,6 @@
 class Demo{
     public static void main(String[] args){
-        int i=10;
-	int j=20;
-        system.out.println(i+","+j);
+	Random rd=new Random();
+        system.out.println(rd.nextLong());
     }
 }
